6a08ce03db2500bee11178b33e34cb7f8ab50a3e
moblifeapps <67682458+moblifeapps@users.noreply.github.com>
2026-01-24 18:37:47 +0000

    ffff
    ffffff

--- mypy.py
+++ mypy.py
@@ -129,6 +129,7 @@ def hasMetaRobotsNoindex(f) :
         window.protag.pageInit();
     });
 </script>-->
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7710644763832143" crossorigin="anonymous"></script>
 </head>", contents, flags=re.I)
             if not m :
                 m = re.search("<body>", contents, flags=re.I)
